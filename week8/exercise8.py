533773cea5b3b89b3a07a70987bfb2cd77286cb9
Juhani Koski <juhanikoski44@gmail.com>
2023-10-22 18:38:09 +0300

    Update exercise8.py

--- week8/exercise8.py
+++ week8/exercise8.py
@@ -1,3 +1,7 @@
+# Video link on youtube
+# https://youtu.be/oq0rIOVTB2A
+
+
 # template
 import tkinter as tk
 import winsound
@@ -31,26 +35,50 @@ def i_suppose_i_have_earned_so_much_points(amount_of_points):
 # Example:
 i_suppose_i_have_earned_so_much_points(0)
 
-#monkey_items = [] # Store monkey canvas items
-#monkey_schedule = None # Variable to store the monkey sound schedule
+monkey_items = [] # Store monkey canvas items
+play_monkey_sounds_flag = True # Flag to control monkey sounds
 
-# define island size and collision range
+# Define islands size and collision range
 island_x = 60
 island_y = 60
-island_collision_x = island_x + 10
-island_collision_y = island_y + 10
+island_collision_x = island_x + 20
+island_collision_y = island_y + 20
 monkey_x=0
 monkey_y=0
 
+# Add each monkey
 def add_monkey(x, y):
     monkey = tk.Canvas(ikkuna, width=monkey_x, height=monkey_y, bg="brown", highlightbackground="brown")
     monkey.place(x=x, y=y)
-    #monkey_items.append(monkey)
+    monkey_items.append(monkey)
     # Play a sound for the monkey
     frequency = random.randint(200, 1000)
     winsound.Beep(frequency, 200)
 
+# Define monkey sound every 10 second
+def play_monkey_sounds():
+    while play_monkey_sounds_flag:
+        time.sleep(10)
+        for monkey in monkey_items:
+            frequency = random.randint(200,1000)
+            winsound.Beep(frequency, 200)
+
+# Start monkey sounds
+def start_monkey_sounds():
+    global play_monkey_sounds_flag
+    play_monkey_sounds_flag = True
+    monkey_sound_thread = threading.Thread(target=play_monkey_sounds)
+    monkey_sound_thread.daemon = True
+    monkey_sound_thread.start()
+
+# Stop monkey sounds
+def stop_monkey_sounds():
+    global play_monkey_sounds_flag
+    play_monkey_sounds_flag = False
+
+# Insert a new island
 def new_island():
+    start_monkey_sounds()
     island = tk.Canvas(ikkuna, width=island_x, height=island_y, bg="sandybrown", highlightbackground="sandybrown")
     while True:
         isl_place_x = random.randint(50, 700-island_collision_x)  # Random x-coordinate within the window
@@ -71,21 +99,25 @@ def new_island():
                 add_monkey(monkey_x, monkey_y)
             break
 
+# Start threading on new island    
 def island_thread():
     thread_islands = threading.Thread(target=new_island)
     thread_islands.start()
-    #thread_monkeys = threading.Thread(target=add_monkey)
-    #thread_monkeys.start()
 
+# Remove all islands and monkey sounds
 def remove_islands():
     for widget in ikkuna.winfo_children():
         if isinstance(widget, tk.Canvas):
             widget.destroy()
+    stop_monkey_sounds()
+    i_suppose_i_have_earned_so_much_points(5)
 
+# Add buttons
 btn_new_island=tk.Button(ikkuna,text="New island", command=island_thread, padx=5, pady=5)
 btn_new_island.place(x=25,y=650)
 
 btn_rmv_island=tk.Button(ikkuna,text="Remove islands", command=remove_islands, padx=5, pady=5)
 btn_rmv_island.place(x=110,y=650)
+
 
 ikkuna.mainloop()
